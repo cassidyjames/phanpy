d1ab9d3e77f4389a87c48d2bc00a5b26db8d1e1c
cassidyjames <c@ssidyjam.es>
2024-04-03 22:38:54 +0000

    Deploying to gh-pages from @ cassidyjames/phanpy@47d7143dae64878e0edf27a3f6e4c979db4080d1 🚀

--- assets/compose-mMw2yUzL.js
+++
@@ -1,8 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./compose-fJddYiNm.js","./vendor-vFSXznL_.js","./index-_Y-Lyw2n.js","./db-09TCbh_p.js","./useTitle-kWYfMHGD.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as u,f as p}from"./useTitle-kWYfMHGD.js";import{U as w,p as _,_ as m,a as e,K as f}from"./vendor-vFSXznL_.js";import{I as S}from"./intl-segmenter-suspense-UPauQu4H.js";const h=w(()=>u(()=>import("./compose-fJddYiNm.js"),__vite__mapDeps([0,1,2,3,4]),import.meta.url));window.opener&&(console=window.opener.console);function g(){var s,a;const[t,r]=_("default"),{editStatus:n,replyToStatus:o,draftStatus:c}=window.__COMPOSE__||{};return p(n?"Editing source status":o?`Replying to @${((s=o.account)==null?void 0:s.acct)||((a=o.account)==null?void 0:a.username)}`:"Compose"),m(()=>{if(t==="closed"){try{window.opener.focus()}catch{}window.close()}},[t]),t==="closed"?e("div",{class:"box",children:[e("p",{children:"You may close this page now."}),e("p",{children:e("button",{onClick:()=>{window.close()},children:"Close window"})})]}):e(S,{children:e(h,{editStatus:n,replyToStatus:o,draftStatus:c,standalone:!0,hasOpener:window.opener,onClose:i=>{const{newStatus:d,fn:l=()=>{}}=i||{};try{d&&window.opener.__STATES__.reloadStatusPage++,l(),r("closed")}catch{}}})})}f(e(g,{}),document.getElementById("app-standalone"));
-//# sourceMappingURL=compose-mMw2yUzL.js.map
--- assets/intl-segmenter-suspense-UPauQu4H.js
+++
@@ -1,8 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./polyfill-force-ihE1cFfj.js","./vendor-vFSXznL_.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{L as e,_ as n}from"./useTitle-kWYfMHGD.js";import{a as t,G as o,p as i,_ as p,$ as f}from"./vendor-vFSXznL_.js";const u=!f();function d({children:a}){if(u)return t(o,{fallback:t(e,{}),children:a});const[l,r]=i(!1);return p(()=>{(async()=>(await n(()=>import("./polyfill-force-ihE1cFfj.js").then(s=>s.p),__vite__mapDeps([0,1]),import.meta.url),r(!0)))()},[]),l?t(o,{fallback:t(e,{}),children:a}):t(e,{})}export{d as I};
-//# sourceMappingURL=intl-segmenter-suspense-UPauQu4H.js.map
