016f236c4506edcc57fb29bf0194b04caffde0cd
cassidyjames <c@ssidyjam.es>
2024-04-19 19:01:51 +0000

    Deploying to gh-pages from @ cassidyjames/phanpy@cb9ca15f8f04f316a145532eb8304ca18018f354 🚀

--- assets/compose-iXd59Q5D.js
+++
@@ -1,8 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./compose-V-zq1y9m.js","./vendor-jXgWaxgC.js","./status-crBXGv3S.js","./useTitle-st0RSW_2.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as u,i as p,I as w}from"./useTitle-st0RSW_2.js";import{U as _,p as m,_ as S,a as e,O as f}from"./vendor-jXgWaxgC.js";const h=_(()=>u(()=>import("./compose-V-zq1y9m.js"),__vite__mapDeps([0,1,2,3]),import.meta.url));window.opener&&(console=window.opener.console);function g(){var s,a;const[t,c]=m("default"),{editStatus:n,replyToStatus:o,draftStatus:r}=window.__COMPOSE__||{};return p(n?"Editing source status":o?`Replying to @${((s=o.account)==null?void 0:s.acct)||((a=o.account)==null?void 0:a.username)}`:"Compose"),S(()=>{if(t==="closed"){try{window.opener.focus()}catch{}window.close()}},[t]),t==="closed"?e("div",{class:"box",children:[e("p",{children:"You may close this page now."}),e("p",{children:e("button",{onClick:()=>{window.close()},children:"Close window"})})]}):e(w,{children:e(h,{editStatus:n,replyToStatus:o,draftStatus:r,standalone:!0,hasOpener:window.opener,onClose:i=>{const{newStatus:d,fn:l=()=>{}}=i||{};try{d&&window.opener.__STATES__.reloadStatusPage++,l(),c("closed")}catch{}}})})}f(e(g,{}),document.getElementById("app-standalone"));
-//# sourceMappingURL=compose-iXd59Q5D.js.map
